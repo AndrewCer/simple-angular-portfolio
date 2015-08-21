f8bb9714b68b47c15025a605ecc331371f5d481f
Andrew Cer <cer.andrew@gmail.com>
2015-08-21 10:55:27 -0600

    got active class to change on click

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,41 @@
-
+app.controller('NavController', function ($scope, $location) {
+  var location = $location.path();
+  $scope.setActive = function (route) {
+    if (route === '/') {
+      $scope.bio = 'not-active'
+      $scope.resume = 'not-active'
+      $scope.projects = 'not-active'
+      $scope.home = 'active'
+    }
+    if (route === '/projects') {
+      $scope.bio = 'not-active'
+      $scope.resume = 'not-active'
+      $scope.home = 'not-active'
+      $scope.projects = 'active'
+    }
+    if (route === '/resume') {
+      $scope.bio = 'not-active'
+      $scope.home = 'not-active'
+      $scope.projects = 'not-active'
+      $scope.resume = 'active'
+    }
+    if (route === '/bio') {
+      $scope.resume = 'not-active'
+      $scope.projects = 'not-active'
+      $scope.home = 'not-active'
+      $scope.bio = 'active'
+    }
+  }
+})
 
 app.controller('HomeController', function ($scope, $location) {
-  $scope.isMain = 'nope';
-  var location = $location.path();
-  if(location === '/'){
-    $scope.isMain = 'active';
-  }
+  // var location = $location.path();
+  // if(location === '/'){
+  //   return $scope.isMain = 'active';
+  // }
 });
 
-app.controller('ProjectsController', function ($scope) {
+app.controller('ProjectsController', function ($scope, $location) {
 
 });
 
