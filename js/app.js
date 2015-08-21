551968748785ed582d2f625b6c9aec3d8b5bba0c
Andrew Cer <cer.andrew@gmail.com>
2015-08-20 18:44:43 -0600

    basic layout set up and basic partials initialized

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-var app = angular.module("appName", ['ngRoute']);
+var app = angular.module("portfolIolIoliooo", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeController',
+      activetab: 'home'
+    })
+    .when('/projects', {
+      templateUrl: 'partials/projects.html',
+      controller: 'ProjectsController',
+      activetab: 'projects'
+    })
+    .when('/bio', {
+      templateUrl: 'partials/bio.html',
+      controller: 'BioController',
+      activetab: 'bio'
+    })
+    .when('/resume', {
+      templateUrl: 'partials/resume.html',
+      controller: 'ResumeController',
+      activetab: 'resume'
+    })
+    .otherwise( {redirectTo: '/'});
+})
---
+++ js/controller.js
@@ -0,0 +1,15 @@
+app.controller('HomeController', function ($scope) {
+
+});
+
+app.controller('ProjectsController', function ($scope) {
+
+});
+
+app.controller('BioController', function ($scope) {
+
+});
+
+app.controller('ResumeController', function ($scope) {
+
+});
